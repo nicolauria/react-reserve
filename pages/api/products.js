70cf4a99ead5e2e591ef122df3c8e5c4957304bb
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-22 15:21:59 -0700

    Create Products Collection, Model Product Data

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,12 @@
 // an api is basically just a bunch of routes (routes are functions)
 // both the frontend and the backend are using es6 syntax
-import products from '../../static/products.json'
+// import products from '../../static/products.json'
 import connectDb from '../../utils/connectDb'
+import Product from '../../models/Product'
 
 connectDb()
 
-export default (req, res) => {
+export default async (req, res) => {
+    const products = await Product.find()
     res.status(200).json(products)
 }
