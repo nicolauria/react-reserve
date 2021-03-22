3957f52da3b5e9246e7423247576940914feb346
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-22 13:48:13 -0700

    Express with API Routes

---
+++ pages/api/products.js
@@ -0,0 +1,7 @@
+// an api is basically just a bunch of routes (routes are functions)
+// both the frontend and the backend are using es6 syntax
+import products from '../../static/products.json'
+
+export default (req, res) => {
+    res.status(200).json(products)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
 function Home() {
+  // used any time we want to perform a side effect (functional programming)
+  React.useEffect(() => {
+    getProducts()
+  }, [])
+
+  async function getProducts() {
+    // both the frontend and the backend run on the same port (we avoid cors errors)
+    const url = 'http://localhost:3000/api/products'
+    const response = await axios.get(url)
+    console.log(response.data)
+  }
+
   return <>home</>;
 }
 
