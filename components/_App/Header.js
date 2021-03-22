d4348c4549e6920a188eaa052a78ec496ce9bcfa
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-22 13:24:56 -0700

    Get Route Data from useRouter Hook, Create Active Links

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,28 +1,34 @@
 import { Menu, Container, Image, Icon } from 'semantic-ui-react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Header() {
+  const router = useRouter()
   const user = false
+
+  function isActive(route) {
+    return route === router.pathname
+  }
   
   return (
     <Menu fluid id="menu" inverted>
       <Container text>
         <Link href="/">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/")}>
             <Image size="mini" src="/static/logo.svg" style={{ marginRight: '1em' }} />
             React Reserve
           </Menu.Item>
         </Link>
 
         <Link href="/cart">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/cart")}>
             <Icon name="cart" size="large" />
             Cart
           </Menu.Item>
         </Link>
 
         {user && <Link href="/create">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/create")}>
             <Icon name="add square" size="large" />
             Create
           </Menu.Item>
@@ -30,7 +36,7 @@ function Header() {
 
         { user ? <>
           <Link href="/account">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/account")}>
             <Icon name="user" size="large" />
             Account
           </Menu.Item>
@@ -43,14 +49,14 @@ function Header() {
         </> :
 
         <><Link href="/login">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/login")}>
             <Icon name="sign in" size="large" />
             Signin
           </Menu.Item>
         </Link>
 
         <Link href="/signup">
-          <Menu.Item header>
+          <Menu.Item header active={isActive("/signup")}>
             <Icon name="signup" size="large" />
             Signup
           </Menu.Item>
