4367c68dd32a8545f28af02084c2102f405f74ed
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-26 14:23:43 -0700

    Structure Cart Page

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,12 @@
+import { Segment } from 'semantic-ui-react'
+import CartItemList from '../components/Cart/CartItemList'
+import CartSummary from '../components/Cart/CartSummary'
+
 function Cart() {
-  return <>cart</>;
+  return <>
+    <CartItemList />
+    <CartSummary />
+  </>;
 }
 
 export default Cart;
