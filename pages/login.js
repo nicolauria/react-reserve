3429dd7ec43e9d61f306ce51993de982b731e536
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-29 10:45:37 -0700

    Add Login Functionality

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react'
 import Link from 'next/link'
 import catchErrors from '../utils/catchErrors'
+import baseUrl from '../utils/baseUrl'
+import axios from 'axios'
+import { handleLogin } from '../utils/auth'
 
 const INITIAL_USER = {
   email: "",
@@ -24,12 +27,15 @@ function Signup() {
     setUser(prevState => ({ ...prevState, [name]: value }))
   }
   
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault()
     try {
-      console.log(user)
       setError('')
       setLoading(true)
+      const url = `${baseUrl}/api/login`
+      const payload = { ...user }
+      const response = await axios.post(url, payload)
+      handleLogin(response.data)
     } catch (error) {
       catchErrors(error, setError)
     } finally {
