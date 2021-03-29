4bf0af954b1617323f40adaa0e8ec1d0f975fa94
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-03-29 10:28:49 -0700

    Validate Post Content on Server

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,12 +2,23 @@ import connectDb from '../../utils/connectDb'
 import User from '../../models/User'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import isEmail from 'validator/lib/isEmail'
+import isLength from 'validator/lib/isLength'
 
 connectDb()
 
 export default async (req, res) => {
     const { name, email, password } = req.body
     try {
+        // validate name, email and password values
+        if (!isLength(name, { min: 3, max: 20 })) {
+            return res.status(422).send('Name must be between 3 and 20 characters')
+        } else if (!isLength(password, { min: 6 })) {
+            return res.status(422).send('Password must be at least 6 characters')
+        } else if (!isEmail(email)) {
+            return res.status(422).send('Email is not valid')
+        }
+        
         const user = await User.findOne({ email })
         if (user) {
             return res.status(422).send("User already exists with email")
